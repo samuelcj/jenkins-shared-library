ac60c28d9846e7daff7044f8e5732ba380daaf8c
samuelcj <samuelcj310@gmail.com>
2025-02-13 20:42:25 +0100

    added parameters

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String DockerRepo ImageTag) {
     env.ENV = input message: "Select the evironment for the deployment", ok: "Done", parameters: [choice(name: "ENV", choices: ["dev", "prod"], description: "Selecting Environment")]
     echo "The  Deployment Environment has been set to ${ENV}"
     echo "Deploying the application..."
     withCredentials([usernamePassword(credentialsId: "DockerHub", passwordVariable: "PASSWORD", usernameVariable: "USERNAME")]) {
-        sh "docker build -t samuelcj310/java-maven-app:java-maven-app_3.0 ."
+        sh "docker build -t $DockerRepo:$ImageTag ."            // Usind varriables that will be specified in the jenkins file
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
         sh "docker push samuelcj310/java-maven-app:java-maven-app_3.0"
     }
